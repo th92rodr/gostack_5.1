47b06133aca4f9cf6838bbbab4f466528055d26a
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-25 20:06:26 -0300

    update eslint and prettier config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard} />
+    <Route path='/' exact component={Dashboard} />
   </Switch>
 );
 
